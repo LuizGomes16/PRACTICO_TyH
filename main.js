09ebd29fd8a354d7d168826793da2b72bb79286b
mmascelloni <140460239+mmascelloni@users.noreply.github.com>
2023-08-04 17:53:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ import {Blockchain} from "./clases/BlockChain.js";
 import {Bloque}     from "./clases/Bloque.js";
 
 //---------------------------------------------------------------------------------------------------------
-//crea nodos por "ahi", que seran los conocidos los nodos que se estan creando, para hacer el broadcasting 
+//Crea nodos por "ahi", que seran los conocidos de los nodos que se estan creando, para hacer el broadcasting 
 //ya que envia los bloques cerrados a los nodos conocidos, y estos lo agregan a su blockchain local
 //---------------------------------------------------------------------------------------------------------
 let nod1 = new Nodo();
@@ -15,7 +15,7 @@ let nod2 = new Nodo();
 //---------------------------------------------------------------------------------------------------------
 let v_ID   = CrearId();                  //el identificador de la transaccion
 let v_ID_token = v_ID;                   //Resguardo el Id que tiene el token (ultima coinbase)
-let v_TKN  = CrearToken();               //id del token, el que es transferido de transaccion a otra. en coinbase se guarda el token.
+let v_TKN  = CrearToken();               //id del token, el que es transferido de transaccion a otra. El coinbase se guarda el token.
 let v_IN   = v_ID;                       //contiene el id de la ultima transaccion que involucra el token
 let v_OUT  = CrearOUT();                 //direccion del nuevo dueño
 let v_st    = "K4804lkn5";
@@ -54,8 +54,8 @@ while (contador <= 1) {
    //console.log(contador);
    v_IN   = v_ID_token;                      //la ultima que involucro el token, la ultima coinbase
    v_ID   = CrearId();                       //nuevo identificador, de la transaccion simple
-   // v_st   = "frweg9080";                  //string de la transaccion simple no aleatorio, para hasherla
-   v_st = GenerarStrAleatorio(12);           //string aleatorio de la transaccion simple, para hasherla
+   // v_st   = "frweg9080";                  //string de la transaccion simple no aleatorio, para hashearla
+   v_st = GenerarStrAleatorio(12);           //string aleatorio de la transaccion simple, para hashearla
    v_OUT= CrearOUT();                        //OUT es de la nueva transaccion, en este caso una normal
    transCoin.putOUT(v_OUT);
    trans = nod.CrearTransaccionSimple(Estrategia, v_st, v_OUT, v_ID, v_hash, v_IN); //genera el hash cuando crea la transaccion simple 
