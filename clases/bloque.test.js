9fe93cc66ef8d9826fa7924b0781d3031db5663e
LuizGomes16 <140459044+LuizGomes16@users.noreply.github.com>
2023-09-13 08:42:58 -0300

    Delete clases directory

--- clases/bloque.test.js
+++
@@ -1,7 +0,0 @@
-import {Bloque } from "./Bloque.js";
-
-test("Test1: Validar un bloque vacio la cantidad de transacciones este en cero", () => { 
-    let bloq = new Bloque();
-    expect(bloq.getCantTran()).toBe(0);    
-});
-
